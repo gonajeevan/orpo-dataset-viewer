f6c42a0469c93f6a12ea240811139fe91299c068
Gona Jeevan <54041004+gonajeevan@users.noreply.github.com>
2024-05-23 11:01:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,30 @@ url = "https://huggingface.co/datasets/mlabonne/orpo-dpo-mix-40k/resolve/main/da
 # Function to load the dataset and cache it
 @st.cache
 def load_data():
-    data = pd.read_parquet(url, engine='pyarrow').sample(n=5000, random_state=1)
+    data = pd.read_parquet(url, engine='pyarrow')
     return data
 
 # Load the dataset
 data = load_data()
+
+# Get unique data source types
+data_source_types = data['source'].unique()
+
+# Streamlit app
+st.title("Response Visualization")
+
+# Dataset credits
+st.markdown("""
+**Dataset Credits:**
+- **Source:** [ORPO-DPO-MIX-40K Dataset on Hugging Face](https://huggingface.co/datasets/mlabonne/orpo-dpo-mix-40k/tree/main/data)
+- **Author:** mlabonne
+""")
+
+# Allow the user to select the data source type
+selected_data_source = st.selectbox("Select Data Source Type", data_source_types)
+
+# Filter the dataset based on the selected data source type
+filtered_data = data[data['source'] == selected_data_source].sample(n=5000, random_state=1)
 
 # Function to parse the chosen and rejected responses with error handling
 def format_conversation(conversation):
@@ -23,27 +42,17 @@ def format_conversation(conversation):
     return formatted_conversation.strip()
 
 # Apply the parsing function to the chosen and rejected columns
-data['chosen_response'] = data['chosen'].apply(format_conversation)
-data['rejected_response'] = data['rejected'].apply(format_conversation)
+filtered_data['chosen_response'] = filtered_data['chosen'].apply(format_conversation)
+filtered_data['rejected_response'] = filtered_data['rejected'].apply(format_conversation)
 
 # Initialize session state for viewed questions
 if 'viewed_questions' not in st.session_state:
     st.session_state.viewed_questions = []
 
-# Streamlit app
-st.title("ORPO Dataset Visualization (Random 5k Samples)")
-
-# Dataset credits
-st.markdown("""
-**Dataset Credits:**
-- **Source:** [ORPO-DPO-MIX-40K Dataset on Hugging Face](https://huggingface.co/datasets/mlabonne/orpo-dpo-mix-40k/tree/main/data)
-- **Author:** mlabonne
-""")
-
 # Create a list of question indices with their viewed status
 question_options = [
     f"{i} - {'Viewed' if i in st.session_state.viewed_questions else 'Not Viewed'}"
-    for i in data.index
+    for i in filtered_data.index
 ]
 
 # Create a dropdown for selecting the question index
@@ -53,7 +62,7 @@ index_selection = st.selectbox("Select a Question Index", question_options)
 selected_index = int(index_selection.split(" - ")[0])
 
 # Get the selected question's details
-selected_data = data.loc[selected_index]
+selected_data = filtered_data.loc[selected_index]
 
 # Update the session state with the new viewed question index
 if selected_index not in st.session_state.viewed_questions:
