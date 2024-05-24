391d7a435d739a5ecc4ec78660ed959bdec034c1
Gona Jeevan <54041004+gonajeevan@users.noreply.github.com>
2024-05-24 08:13:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,8 +102,13 @@ selected_data = filtered_data.loc[index_selection]
 # st.markdown("### Data Source Type:")
 # st.markdown(f"**{selected_data['source']}**")
 if selected_data_source.lower() in ('toxic-dpo-v0.2'):
-    st.markdown('**Note: ORPO-DPO-mix-40k contains a dataset (toxic-dpo-v0.2) designed to prompt the model to answer illegal questions.**')
-    st.markdown('**You can remove it as follows: data = data.filter(lambda r: r["source"] != "toxic-dpo-v0.2")**')
+    st.markdown("""
+    <div style='border: 1px solid black; padding: 10px;'>
+        <strong>Note: ORPO-DPO-mix-40k contains a dataset (toxic-dpo-v0.2) designed to prompt the model to answer illegal questions.</strong>
+        <br>
+        <strong>You can remove it as follows: data = data.filter(lambda r: r["source"] != "toxic-dpo-v0.2")</strong>
+    </div>
+    """, unsafe_allow_html=True)
 
 st.markdown("### Question:")
 st.markdown(f"**{selected_data['prompt']}**")
